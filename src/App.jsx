f528649781f5544b4666e690b79fbca9e0686804
Jaime Gil <jgil13.code@gmail.com>
2022-07-08 19:03:29 -0500

    código inicial de pelicula web

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import styles from "./App.module.css";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
+
+export function App() {
+  return (
+    <Router>
+      <header>
+        <Link to="/">
+          <h1 className={styles.title}> Movies </h1>
+        </Link>
+      </header>
+      <main>
+        <Switch>
+          <Route exact path="/movies/:movieId">
+            <MovieDetails />
+          </Route>
+          <Route path="/">
+            <LandingPage></LandingPage>
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  );
+}
